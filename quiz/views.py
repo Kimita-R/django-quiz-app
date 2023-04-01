6ab69a367526833a900ffccdd9298b4f11c5d05f
Kimita <kimitaramalingum@gmail.com>
2023-04-01 14:59:31 +0200

    Docstrings created for quiz/views.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,19 +1,73 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Option, Quiz, Result
 from datetime import datetime
+"""
+This module contains the view functions for myquizsite django web application
+
+Each view function corresponds to a different URL endpoint in the application, and is responsible for handling incoming
+HTTP requests and returning an HTTP response.
+
+Functions:
+    index(request):
+        Renders the home page of the application.
+
+    topic(request, quiz_id):
+        Renders a topic page for each available quiz in the app.
+
+    results(request, quiz_id):
+        Renders the results of a submitted quiz
+
+"""
 
 # Create your views here.
 def index(request):
+     """This method renders the home page of the app
+        Each available topic that corresponds to a 
+        quiz is displayed. 
+
+        :param HttpRequest request
+        
+        :returns: Rendered index.html (home) page
+
+        :rtype: HttpResponse object
+    """
     quiz_list = Quiz.objects.order_by('pub_date')[:5]
     return render(request, 'quiz/index.html', {'quiz_list': quiz_list})
 
 def topic(request, quiz_id):
+    """This method renders the topic page that 
+        corresponds to the given quiz_id. 
+        The content that relates to the quiz is 
+        displayed as well as the quiz for the user 
+        to test their knowledge. 
+
+        :param HttpRequest request
+        :param int quiz_id
+        
+        :returns: Rendered topic.html page
+
+        :rtype: HttpResponse object
+    """
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     user = request.user
     results = Result.objects.filter(quiz=quiz_id, user=user)
     return render(request, 'quiz/topic.html', {'quiz': quiz, 'results': results})
 
 def results(request, quiz_id):
+    """This method renders the results page once 
+        a user has answered a quiz and submitted their answers.
+
+        If the user is completing the quiz for the first time 
+        or achieves a new high score this score is saved 
+        to the database.
+
+        :param HttpRequest request
+        :param int quiz_id
+        
+        :returns: Rendered results.html page
+
+        :rtype: HttpResponse object
+    """
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     user = request.user
     # Get list of questions associated with current quiz
